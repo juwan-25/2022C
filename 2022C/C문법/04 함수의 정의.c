db118e935e1cf9e4cb1c919c647226e2d19ffbd8
juwan-25 <s2109@e-mirim.hs.kr>
2022-03-17 11:46:33 +0900

    <파일 분할> 함수 선언 : 헤더 함수 정의 : C

--- 2022C/C문법/04 함수의 정의.c
+++ 2022C/C문법/04 함수의 정의.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
-int sum(int a, int b); //함수의 선언 : 함수를 먼저 선언해야 에러 발생X
+#include "04.h"
 
 int main(void)
 {
 	int s = sum(1, 2);
 	printf("%d", s);
 	return 0;
-}
-
-int sum(int a, int b)
-{
-	return a + b;
 }
---
+++ 2022C/C문법/04.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+//함수의 선언 : 함수를 먼저 선언해야 에러 발생X
+int sum(int a, int b); 
