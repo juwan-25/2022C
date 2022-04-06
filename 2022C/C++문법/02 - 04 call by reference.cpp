47a24a21d27aca5374ed1d922e3c8616ad5c9f98
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-06 10:06:21 +0900

    call by reference(참조)

--- 2022C/C++문법/02 - 04 call by reference.cpp
+++ 2022C/C++문법/02 - 04 call by reference.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void puls(int* num)
+//void puls(int* num)
+//{
+//	*num += 2;
+//}
+
+void puls(int& num)
 {
-	*num += 2;
+	num += 2;
 }
 
 int main(void)
 {
 	int a = 3;
-	puls(&a);
+	puls(a);
 	cout << a << endl;
+
+	
 
 	return 0;
 }
