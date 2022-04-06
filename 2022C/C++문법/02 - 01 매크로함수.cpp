40f6b67014ff0011a645f4e3da97db5ae347783c
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-06 09:09:01 +0900

    매크로함수 완성

--- 2022C/C++문법/02 - 01 매크로함수.cpp
+++ 2022C/C++문법/02 - 01 매크로함수.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 //#이 들어가면 컴파일러가 아니라 전처리기에서 프리 컴파일러가 처리
 //자료형 영향X
-#define jegob(A) A*A
+#define jegob(A) ((A)*(A))
 
 int main(void)
 {
-	//연산자 우선순위로 인해 의도하지 않은 결과가 나옴
-	cout << jegob(3+1) << endl; // 7
-	cout << 3+1 * 3+1 << endl; // 7
+	cout << jegob(3+1) << endl; // 16
+	cout << ((3+1) * (3+1)) << endl; // 16
 
 	return 0;
 }
