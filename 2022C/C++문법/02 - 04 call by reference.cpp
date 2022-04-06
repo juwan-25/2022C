5fa5b18793f1c51f1cb6c19c0523330808240740
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-06 10:14:24 +0900

    참조 추가 설명

--- 2022C/C++문법/02 - 04 call by reference.cpp
+++ 2022C/C++문법/02 - 04 call by reference.cpp
@@ -6,6 +6,12 @@ using namespace std;
 //	*num += 2;
 //}
 
+//p.72 **
+//참조는 반드시 변수를 가리켜야 함
+//상수를 참조X, 주소값 참조X, null과 nullptr로 초기화X
+//한 번 참조했던 참조자는 다른 변수 참조X -> 바꿀 수 없음
+
+
 void puls(int& num)
 {
 	num += 2;
@@ -16,8 +22,6 @@ int main(void)
 	int a = 3;
 	puls(a);
 	cout << a << endl;
-
-	
 
 	return 0;
 }
