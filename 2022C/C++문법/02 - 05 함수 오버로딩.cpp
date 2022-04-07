1000c49f67114629ddb70d74971c26a75b282ef8
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-07 10:59:50 +0900

    디폴트 매개변수(함수 오버로딩과 다름)

--- 2022C/C++문법/02 - 05 함수 오버로딩.cpp
+++ 2022C/C++문법/02 - 05 함수 오버로딩.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
-int jegob(int a, int b)
+//디폴트 값, 변수 값을 넘겨 주지 않았을 때 자동으로 기본값 실행
+int jegob(int a, int b=1)
 {
 	return a * a + b;
 }
-double jegob(double a, double b)
+
+//어떤 함수를 실행해야 할지 모호해서 오류 발생
+//int jegob(int a)
+//{
+//	return a * a;
+//}
+
+double jegob(double a, double b=1.1)
 {
 	return a * a + b;
 }
@@ -14,8 +21,8 @@ double jegob(double a, double b)
 int main(void)
 {
 	//함수 오버로딩 : 이름은 같으나 매개변수가 다름
-	cout << jegob(4,1) << endl; //17
-	cout << jegob(3.3, 1.1) << endl; //11.99
+	cout << jegob(4) << endl; //17
+	cout << jegob(3.3) << endl; //11.99
 
 	return 0;
 }
