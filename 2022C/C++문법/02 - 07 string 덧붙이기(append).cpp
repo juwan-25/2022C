9697ea897a70b09c95e700fc80413b60befc261b
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-07 11:40:20 +0900

    string 덧붙이기

---
+++ 2022C/C++문법/02 - 07 string 덧붙이기(append).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string> //string 클래스를 가지고 있음
+#include <stdio.h>
+#include <string.h> //strcpy()를 가지고 있음
+using namespace std;
+
+int main(void)
+{
+	char cMunja[20];
+	strcpy(cMunja, "c_insert");
+	strcat(cMunja, "++");
+	printf("%s\n", cMunja);
+
+	std::string cppMunja;
+	cppMunja = "cpp_insert";
+	cppMunja += "++";
+	cout << cppMunja << endl;
+
+	return 0;
+}
