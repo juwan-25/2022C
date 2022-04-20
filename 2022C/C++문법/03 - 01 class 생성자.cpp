ab4d06ef2f7df6a62a6165096cda154ae91f4997
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-20 10:01:40 +0900

    함수 오버로딩

--- 2022C/C++문법/03 - 01 class 생성자.cpp
+++ 2022C/C++문법/03 - 01 class 생성자.cpp
@@ -12,7 +12,7 @@ private:
 public:
 	//생성자 : 객체가 생성될 때, 호출되는 함수
 	Student();
-
+	Student(int Hakbun, string Name);
 	void show();
 };
 
@@ -21,7 +21,7 @@ int main(void)
 	Student stu1;
 	stu1.show();
 
-	Student stu2 = Student();
+	Student stu2 = Student(1111,"JWP");
 	stu2.show();
 
 	return 0;
@@ -34,8 +34,15 @@ Student::Student()
 	cout << "학번이 등록되었습니다." << endl;
 }
 
+Student::Student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+}
+
 void Student::show()
 {
-	cout << "학번은" << nHakbun << "입니다." << endl;
-	cout << "이름은" << sName << "입니다." << endl << endl;
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
