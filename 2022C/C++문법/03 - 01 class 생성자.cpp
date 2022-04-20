b060d15bd0127759e6a4ccc75b10fd2e536dc3c7
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-20 09:41:55 +0900

    멤버함수 추가

--- 2022C/C++문법/03 - 01 class 생성자.cpp
+++ 2022C/C++문법/03 - 01 class 생성자.cpp
@@ -15,14 +15,23 @@ public:
 	{
 		nHakbun = 1234;
 		sName = "이사람";
-		cout << "학번이 등록되었습니다."<<endl;
+		cout << "학번이 등록되었습니다." << endl;
+	}
+
+	void show()
+	{
+		cout << "학번은" << nHakbun << "입니다." << endl;
+		cout << "이름은" << sName << "입니다." << endl << endl;
 	}
 };
 
 int main(void)
 {
 	Student stu1;
+	stu1.show();
+
 	Student stu2 = Student();
+	stu2.show();
 
 	return 0;
 }
