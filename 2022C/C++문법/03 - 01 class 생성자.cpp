39c27bdb75286448bab49f3e2e8d224003b4f998
juwan-25 <s2109@e-mirim.hs.kr>
2022-05-11 10:02:06 +0900

    객체 멤버 변수 초기화

--- 2022C/C++문법/03 - 01 class 생성자.cpp
+++ 2022C/C++문법/03 - 01 class 생성자.cpp
@@ -29,15 +29,16 @@ int main(void)
 
 Student::Student()
 {
-	nHakbun = 1234;
-	sName = "Hong";
+	this->nHakbun = 1234;
+	this->sName = "Hong";
 	cout << "학번이 등록되었습니다." << endl;
 }
 
 Student::Student(int Hakbun, string Name)
+	: nHakbun(Hakbun), sName(Name)
+	//객체 생성과 동시에 멤버변수 초기화 -> 멤버변수(매개변수)
+	// const/참조형 멤버변수를 사용할 수 있음
 {
-	this->nHakbun = Hakbun;
-	this->sName = Name;
 	cout << "학번이 등록되었습니다." << endl;
 }
 
