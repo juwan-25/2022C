95057142034fb84485325d4e6c857e66ae124d92
juwan-25 <s2109@e-mirim.hs.kr>
2022-05-12 12:15:13 +0900

    전통적인 문자열 방법으로 생성자 변경

--- 2022C/C++문법/03 - 01 class 생성자.cpp
+++ 2022C/C++문법/03 - 01 class 생성자.cpp
@@ -7,40 +7,34 @@ class Student
 {
 private:
 	int nHakbun;
-	string sName;
+	char* sName;
 
 public:
 	//생성자 : 객체가 생성될 때, 호출되는 함수
 	Student();
-	Student(int Hakbun, string Name);
+	Student(int Hakbun, const char* Name);
 	void show();
 };
 
 int main(void)
 {
-	Student stu1;
+	Student stu1 = Student(2219, "Zeus");
 	stu1.show();
-
-	Student stu2 = Student(2219,"Zeus");
-	stu2.show();
 
 	return 0;
 }
 
-Student::Student()
-{
-	this->nHakbun = 1234;
-	this->sName = "Hong";
-	cout << "학번이 등록되었습니다." << endl;
-}
-
-Student::Student(int Hakbun, string Name)
-	: nHakbun(Hakbun), sName(Name)
+Student::Student(int Hakbun, const char* Name)
+	: nHakbun(Hakbun)
 	//객체 생성과 동시에 멤버변수 초기화 -> 멤버변수(매개변수)
 	// const/참조형 멤버변수를 사용할 수 있음
 {
+	int len = strlen(Name)+1;
+	sName = new char[len];
+	strcpy(sName, Name);
 	cout << "학번이 등록되었습니다." << endl;
 }
+
 
 void Student::show()
 {
